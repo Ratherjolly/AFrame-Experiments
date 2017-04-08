5574c01b42a7e2fe600e19b3828afcec4c031fe1
Matt <msponholz@herzing.edu>
2017-04-08 11:12:33 -0500

    Update BattleRockAR.js

--- Scripts/BattleRockAR.js
+++ Scripts/BattleRockAR.js
@@ -22,7 +22,7 @@ AFRAME.registerComponent('click-listener', {
 				mdl.setAttribute('scale', '0.01 0.01 0.01');
 				//mdl.setAttribute('position', '0 0 0');
 			    	mdl.innerHTML = "<a-animation attribute='rotation' dur='8000' easing='linear' repeat='indefinite' to='0 360 0'></a-animation>";
-				txt.setAttribute('text-geometry', 'value: Bear Lancer');
+				txt.setAttribute('text-geometry', 'value:');
 				txt.setAttribute('position', '-1.5 0 1.5');
 			    }
 			    else if (i==2) {
@@ -30,13 +30,13 @@ AFRAME.registerComponent('click-listener', {
 				mdl.setAttribute('scale', '0.25 0.25 0.25');
 				//mdl.setAttribute('position', '0 0 0');
 				mdl.innerHTML = "<a-animation attribute='rotation' dur='8000' easing='linear' repeat='indefinite' to='0 360 0'></a-animation><a-entity position='0 0 0' particle-system='preset: rain; color: #EF0000,#44CC00'></a-entity>";
-				txt.setAttribute('text-geometry', 'value: The Brawler');
+				txt.setAttribute('text-geometry', 'value:');
 				txt.setAttribute('position', '-1.8 0 1.5');
 			    }
 			    else if (i==3) {
 				mdl.setAttribute('obj-model', 'obj: #cube-obj; mtl: #cube-mtl;');
 				mdl.setAttribute('scale', '0.05 0.05 0.05');
-				txt.setAttribute('text-geometry', 'value: The Cube');
+				txt.setAttribute('text-geometry', 'value:');
 				txt.setAttribute('position', '-1.4 0 1.5');
 			    	mdl.innerHTML = "<a-animation attribute='rotation' dur='8000' easing='linear' repeat='indefinite' to='0 360 0'></a-animation><a-entity position='0 0 0' particle-system='preset: rain; color: #EF0000,#44CC00'></a-entity><a-entity position='0 0 0' particle-system='color: #EF0000,#44CC00'></a-entity>";
 			    }
