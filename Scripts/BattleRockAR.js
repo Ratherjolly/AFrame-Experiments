628eb857656c0da21fa7286a9bd7cd0e31832eed
Matt <msponholz@herzing.edu>
2017-04-07 23:02:57 -0500

    Update BattleRockAR.js

--- Scripts/BattleRockAR.js
+++ Scripts/BattleRockAR.js
@@ -19,28 +19,30 @@ AFRAME.registerComponent('click-listener', {
 				i = 1;
 			    MI.innerHTML = i;
 			    if (i==1) {
-				//mdl.setAttribute('obj-loader', 'url(./Objects/LancerBear.obj)');
 				mdl.setAttribute('obj-model', 'obj: #bear-obj;');//'obj:Objects/LancerBear.obj');
+				mdl.innerHTML = "<a-animation attribute="rotation" dur="10000" easing="linear" repeat="indefinite" to="0 360 0"></a-animation>";
 				mdl.setAttribute('scale', '0.01 0.01 0.01');
 				mdl.setAttribute('position', '0 0 0');
 				txt.setAttribute('text-geometry', 'value: Bear Lancer');
 				txt.setAttribute('position', '-1.5 0 1.5');
+			    	mdl.innerHTML = "";
 			    }
 			    else if (i==2) {
-				//mdl.setAttribute('obj-loader', 'url(./Objects/BrawlerSmoothFists.obj)');
 				mdl.setAttribute('obj-model', 'obj: #brawler-obj;');
 				mdl.setAttribute('scale', '0.25 0.25 0.25');
 				mdl.setAttribute('position', '0 0 0');
 				txt.setAttribute('text-geometry', 'value: The Brawler');
 				txt.setAttribute('position', '-1.8 0 1.5');
+			    	mdl.innerHTML = "";
 			    }
 			    else if (i==3) {
-				//mdl.setAttribute('obj-loader', 'url(./Objects/DynamoSitting1.obj)');
 				mdl.setAttribute('obj-model', 'obj: #cube-obj; mtl: #cube-mtl;');
 				mdl.setAttribute('scale', '0.05 0.05 0.05');
 				//mdl.setAttribute('position', '0 0 -2.5');
 				txt.setAttribute('text-geometry', 'value: BATTLEROCK STUDIOS');
 				txt.setAttribute('position', '-2.0 0 1.5');
+			    	mdl.innerHTML = "<a-animation attribute="rotation" dur="10000" easing="linear" repeat="indefinite" to="0 360 0"></a-animation>";
+				
 			    }
 		    });
 		  }
